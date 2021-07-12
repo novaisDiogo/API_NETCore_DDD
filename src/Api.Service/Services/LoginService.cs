6db40892d36c8872fa60e4b0007faa3c7cf1229e
Diogo Freitas <diogo.freitas@hotmail.com>
2021-07-12 20:50:50 -0300

    Gerar Token

--- src/Api.Service/Services/LoginService.cs
+++ src/Api.Service/Services/LoginService.cs
@@ -1,28 +1,109 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using Api.Domain.Dtos;
+using Api.Domain.Entities;
 using Api.Domain.Interfaces.Services.User;
 using Api.Domain.Repository;
+using Api.Domain.Security;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Api.Service.Services
 {
     public class LoginService : ILoginService
     {
         private IUserRepository _repository;
-        public LoginService(IUserRepository repository)
+        private SigningConfigurations _signinConfigurations;
+        private TokenConfigurations _tokenConfigurations;
+
+        private IConfiguration _configuration;
+
+        public LoginService(IUserRepository repository,
+                            SigningConfigurations signinConfigurations,
+                            TokenConfigurations tokenConfiguration,
+                            IConfiguration configuration)
         {
+            _signinConfigurations = signinConfigurations;
+            _tokenConfigurations = tokenConfiguration;
             _repository = repository;
+            _configuration = configuration;
         }
 
         public async Task<object> FindByLogin(LoginDto user)
         {
+            var baseUser = new UserEntity();
+
             if (user != null && !string.IsNullOrWhiteSpace(user.Email))
             {
-                return await _repository.FindByLogin(user.Email);
+                baseUser = await _repository.FindByLogin(user.Email);
+
+                if (baseUser == null)
+                {
+                    return new
+                    {
+                        authenticated = false,
+                        message = "Falha ao autenticar"
+                    };
+                }
+                else
+                {
+                    ClaimsIdentity identity = new ClaimsIdentity(
+                        new GenericIdentity(baseUser.Email),
+                        new[]
+                        {
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),//jti O id do token
+                            new Claim(JwtRegisteredClaimNames.UniqueName, user.Email)
+                        }
+                    );
+
+                    DateTime createDate = DateTime.Now;
+                    DateTime expirationDate = createDate + TimeSpan.FromSeconds(_tokenConfigurations.Seconds);
+
+                    var handler = new JwtSecurityTokenHandler();
+
+                    string token = CreateToken(identity, createDate, expirationDate, handler);
+
+                    return SuccessObject(createDate, expirationDate, token, user);
+                }
             }
             else
             {
                 return null;
             }
+        }
+
+        private string CreateToken(ClaimsIdentity identity, DateTime createDate, DateTime expirationDate,
+                                    JwtSecurityTokenHandler handler)
+        {
+            var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _tokenConfigurations.Issuer,
+                Audience = _tokenConfigurations.Audience,
+                SigningCredentials = _signinConfigurations.SigningCredentials,
+                Subject = identity,
+                NotBefore = createDate,
+                Expires = expirationDate
+            });
+
+            var token = handler.WriteToken(securityToken);
+
+            return token;
+        }
+
+        private object SuccessObject(DateTime createDate, DateTime expirationDate, string token, LoginDto user)
+        {
+            return new
+            {
+                authenticated = true,
+                created = createDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                expiration = expirationDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                accessToken = token,
+                userName = user.Email,
+                message = "Usuario Logado com sucesso"
+            };
         }
     }
 }
