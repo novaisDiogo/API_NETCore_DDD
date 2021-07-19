26a0672a6cc6e20ca6825b7cdf3db136d30aaca8
Diogo Freitas <diogo.freitas@hotmail.com>
2021-07-19 20:54:00 -0300

    UserDto, UserModel and AutoMapper

---
+++ src/Api.CrossCutting/Mappings/DtoToModelProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Domain.Dtos.User;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrossCutting.Mappings
+{
+    public class DtoToModelProfile : Profile
+    {
+        public DtoToModelProfile()
+        {
+            CreateMap<UserModel, UserDto>()
+                .ReverseMap();
+        }
+    }
+}
---
+++ src/Api.CrossCutting/Mappings/EntityToDtoProfile.cs
@@ -0,0 +1,24 @@
+﻿using Api.Domain.Entities;
+using AutoMapper;
+using Domain.Dtos.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrossCutting.Mappings
+{
+    public class EntityToDtoProfile : Profile
+    {
+        public EntityToDtoProfile()
+        {
+            CreateMap<UserDto, UserEntity>()
+                .ReverseMap();
+
+            CreateMap<UserDtoCreateResult, UserEntity>()
+                .ReverseMap();
+
+            CreateMap<UserDtoUpdateResult, UserEntity>()
+                .ReverseMap();
+        }
+    }
+}
