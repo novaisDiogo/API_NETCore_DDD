8266e3602d661bb804b078f4b847f26656740d80
Diogo Freitas <diogo.freitas@hotmail.com>
2021-08-03 20:32:05 -0300

    Unit Tests Api.Data | BaseTest

---
+++ src/Api.Data.Test/BaseTest.cs
@@ -0,0 +1,45 @@
+﻿using Api.Data.Context;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Test
+{
+    public abstract class BaseTest
+    {
+        public BaseTest()
+        {
+
+        }
+    }
+
+    public class DbTest : IDisposable
+    {
+        private string dataBaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        public DbTest()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<MyContext>(o =>
+            {
+                o.UseSqlServer($"Data Source=DESKTOP-MNKOETH;Initial Catalog={dataBaseName};Integrated Security=True");
+            }, ServiceLifetime.Transient);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        public void Dispose()
+        {
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureDeleted();
+            }
+        }
+    }
+}
