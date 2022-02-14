09de7bbf02fcbdd205f51efe905b47683a88bf53
Diogo Freitas <diogo.freitas@hotmail.com>
2022-02-14 20:03:40 -0300

    Integration getAll

--- src/Api.Integration.Test/User/QuandoRequisitarUsuario.cs
+++ src/Api.Integration.Test/User/QuandoRequisitarUsuario.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,13 +15,16 @@ namespace Api.Integration.Test.User
         private string _name { get; set; }
         private string _email { get; set; }
 
+        public QuandoRequisitarUsuario()
+        {
+            _name = Faker.Name.First();
+            _email = Faker.Internet.Email();
+        }
+
         [Fact]
         public async Task E_possivel_realizar_crud_usuario()
         {
             await AddToken();
-
-            _name = Faker.Name.First();
-            _email = Faker.Internet.Email();
 
             var userDto = new UserDtoCreate()
             {
@@ -28,6 +32,7 @@ namespace Api.Integration.Test.User
                 Email = _email
             };
 
+            //POST
             var response = await PostJsonAsync(userDto, $"{hostApi}users", client);
             var postContent = await response.Content.ReadAsStringAsync();
             var postResult = JsonConvert.DeserializeObject<UserDtoCreateResult>(postContent);
@@ -36,6 +41,15 @@ namespace Api.Integration.Test.User
             Assert.Equal(_name, postResult.Name);
             Assert.Equal(_email, postResult.Email);
             Assert.True(postResult.Id != default(Guid));
+
+            //GETALL
+            response = await client.GetAsync($"{hostApi}users");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var jsonResult = await response.Content.ReadAsStringAsync();
+            var listFromJson = JsonConvert.DeserializeObject<IEnumerable<UserDto>>(jsonResult);
+            Assert.True(listFromJson.Count() > 0);
+            Assert.True(listFromJson.Where(c => c.Id == postResult.Id).Count() == 1);
         }
     }
 }
