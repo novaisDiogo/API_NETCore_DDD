22ce5408571bce1736a903f591e8c470dd94b780
Diogo Freitas <diogo.freitas@hotmail.com>
2022-02-16 19:24:52 -0300

    Integration get, put, delete

--- src/Api.Integration.Test/User/QuandoRequisitarUsuario.cs
+++ src/Api.Integration.Test/User/QuandoRequisitarUsuario.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -50,6 +51,45 @@ namespace Api.Integration.Test.User
             var listFromJson = JsonConvert.DeserializeObject<IEnumerable<UserDto>>(jsonResult);
             Assert.True(listFromJson.Count() > 0);
             Assert.True(listFromJson.Where(c => c.Id == postResult.Id).Count() == 1);
+
+            //PUT
+            var updateUserDto = new UserDtoUpdate()
+            {
+                Id = postResult.Id,
+                Name = Faker.Name.First(),
+                Email = Faker.Internet.Email()
+            };
+
+            var stringContent = new StringContent(JsonConvert.SerializeObject(updateUserDto),
+                Encoding.UTF8, "application/json");
+
+            response = await client.PutAsync($"{hostApi}users", stringContent);
+            jsonResult = await response.Content.ReadAsStringAsync();
+            var updatedResult = JsonConvert.DeserializeObject<UserDtoUpdateResult>(jsonResult);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotEqual(postResult.Name, updatedResult.Name);
+            Assert.NotEqual(postResult.Email, updatedResult.Email);
+
+            //GETBYID
+
+            response = await client.GetAsync($"{hostApi}users/{updatedResult.Id}");
+            jsonResult = await response.Content.ReadAsStringAsync();
+            var getResult = JsonConvert.DeserializeObject<UserDto>(jsonResult);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotNull(getResult);
+            Assert.Equal(getResult.Name, updatedResult.Name);
+            Assert.Equal(getResult.Email, updatedResult.Email);
+
+            //DELETE
+
+            response = await client.DeleteAsync($"{hostApi}users/{updatedResult.Id}");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            //GET ID, CHECK DELETE
+            response = await client.GetAsync($"{hostApi}users/{updatedResult.Id}");
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
         }
     }
 }
