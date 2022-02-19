a0175e4e06b7b2f515a450d0b9a06d1b71a32c23
Diogo Freitas <diogo.freitas@hotmail.com>
2022-02-19 15:26:11 -0300

    Implementation (UF, MUNICIPIO, CEP) entities

---
+++ src/Api.Domain/Entities/CepEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class CepEntity
+    {
+        [Required]
+        [MaxLength(10)]
+        public string Cep { get; set; }
+        [Required]
+        [MaxLength(60)]
+        public string Logradouro { get; set; }
+        [MaxLength(10)]
+        public string Numero { get; set; }
+        [Required]
+        public Guid MunicipioId { get; set; }
+        public MunicipioEntity Municipio { get; set; }
+    }
+}
---
+++ src/Api.Domain/Entities/UfEntity.cs
@@ -0,0 +1,19 @@
+﻿using Api.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class UfEntity : BaseEntity
+    {
+        [Required]
+        [MaxLength(2)]
+        public string Sigla { get; set; }
+        [Required]
+        [MaxLength(45)]
+        public string Nome { get; set; }
+        public IEnumerable<MunicipioEntity> Municipios { get; set; }
+    }
+}
