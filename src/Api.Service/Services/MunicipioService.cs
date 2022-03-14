56677614de983ff76faa4e7a4acb4080d29f6f18
Diogo Freitas <diogo.freitas@hotmail.com>
2022-03-14 19:53:54 -0300

    services (cep, municipio, uf)

---
+++ src/Api.Service/Services/MunicipioService.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using Domain.Dtos.Municipio;
+using Domain.Entities;
+using Domain.Interfaces.Services.Municipio;
+using Domain.Models;
+using Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services
+{
+    public class MunicipioService : IMunicipioService
+    {
+        private IMunicipioRepository _repository;
+        private readonly IMapper _mapper;
+
+        public MunicipioService(IMunicipioRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<MunicipioDto> Get(Guid id)
+        {
+            var entity = await _repository.SelectAsync(id);
+            return _mapper.Map<MunicipioDto>(entity);
+        }
+
+        public async Task<IEnumerable<MunicipioDto>> GetAll()
+        {
+            var entity = await _repository.SelectAsync();
+            return _mapper.Map<IEnumerable<MunicipioDto>>(entity);
+        }
+
+        public async Task<MunicipioDtoCompleto> GetCompleteByIBGE(int codIBGE)
+        {
+            var entity = await _repository.GetCompleteByIBGE(codIBGE);
+            return _mapper.Map<MunicipioDtoCompleto>(entity);
+        }
+
+        public async Task<MunicipioDtoCompleto> GetCompleteById(Guid id)
+        {
+            var entity = await _repository.GetCompleteById(id);
+            return _mapper.Map<MunicipioDtoCompleto>(entity);
+        }
+
+        public async Task<MunicipioDtoCreateResult> Post(MunicipioDtoCreate municipio)
+        {
+            var model = _mapper.Map<MunicipioModel>(municipio);
+            var entity = _mapper.Map<MunicipioEntity>(model);
+            var result = await _repository.InsertAsync(entity);
+
+            return _mapper.Map<MunicipioDtoCreateResult>(result);
+        }
+
+        public async Task<MunicipioDtoUpdateResult> Put(MunicipioDtoUpdate municipio)
+        {
+            var model = _mapper.Map<MunicipioModel>(municipio);
+            var entity = _mapper.Map<MunicipioEntity>(model);
+            var result = await _repository.UpdateAsync(entity);
+
+            return _mapper.Map<MunicipioDtoUpdateResult>(result);
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+    }
+}
